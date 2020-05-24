329c1d1e236e1976393e90a3804a0f2728e42f20
HasniMehdi <hasnimehdi@outlook.com>
2020-05-24 03:52:29 +0200

    word count exapmle

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,7 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.util.Arrays;
 
@@ -14,8 +15,13 @@ public class Test {
         context.textFile("src/main/resources/data/input.txt")
                 .flatMap(x -> Arrays.asList(x.split(" ")).iterator())
                 .filter(x -> x.length() > 1 && !x.equals("-->") && !x.matches("[0-9]{2}:[0-9]{2}.[0-9]{3}"))
-                .map(v -> v.replace(".", "").replace("'", "").replaceAll("[0-9]{1,10}",""))
-                .foreach(x -> System.out.println(x));
+                .map(v -> v.replace(".", "").replace("'", "").replaceAll("[0-9]{1,10}", ""))
+                .mapToPair(v -> new Tuple2<>(v, 1L))
+                .reduceByKey((e, v) -> e + v)
+                .mapToPair(v -> new Tuple2<>(v._2, v._1))
+                .filter(v -> v._1 > 10 && v._2.length() > 4)
+                .sortByKey(true)
+                .foreach(x -> System.out.println(x._2 + ": " + x._1));
         context.close();
 
     }
